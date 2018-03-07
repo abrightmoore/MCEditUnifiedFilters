59481faeaaf2f2e96ded43a0b2057e9157abf2f5
Adrian Brightmoore <abrightmoore@yahoo.com.au>
2018-03-07 13:29:56 +1100

    Depress the landscape

---
+++ Crater_v3.py
@@ -0,0 +1,390 @@
+# This filter is for generating a Crater
+# abrightmoore@yahoo.com.au
+# http://brightmoore.net
+# My filters may include code and inspiration from PYMCLEVEL/MCEDIT mentors @Texelelf, @Sethbling, @CodeWarrior0, @Podshot_
+
+import time # for timing
+from math import sqrt, tan, sin, cos, pi, ceil, floor, acos, atan, asin, degrees, radians, log, atan2
+from random import *
+from numpy import *
+from mcplatform import *
+from os import listdir
+from os.path import isfile, join
+import glob
+from pymclevel import TAG_List, TAG_Byte, TAG_Int, TAG_Compound, TAG_Short, TAG_Float, TAG_Double, TAG_String, TAG_Long
+from pymclevel import TAG_String # @Texelelf
+from pymclevel import TileEntity # @Texelelf
+from pymclevel import alphaMaterials, MCSchematic, MCLevel, BoundingBox, MCMaterials
+# import from @Texelelf
+from copy import deepcopy
+import bresenham # @Codewarrior0
+from random import Random # @Codewarrior0
+import inspect # @Texelelf
+from PIL import Image
+import png
+
+# GLOBAL
+CHUNKSIZE = 16
+MAXANGLES = 360
+a = 2*pi/MAXANGLES
+AIR = (0,0)
+LAVA = (11,0)
+
+coloursList = "15 7 8 0 6 2 10 11 3 9 13 5 4 1 14 12".split() # I like this sequence
+#coloursList = "15 15 15 15 15 11 11 11 0".split()
+colours = map(int, coloursList)
+palette = []
+palette.append( [ (1,0),(1,1), (1,2), (1,3), (1,4), (1,5), (1,6), (80,0) ] )
+palette.append( [ (95,3),(22,0), (24,2), (19,0), (18,0), (3,0), (1,0), (80,0) ]	)
+RAND2 = Random(42)
+
+# Choose a colour pallette
+print 'Pallette Selection'
+gap = RAND2.randint(1,4)
+for i in range(32):
+	newCols = []
+	baseIndex = RAND2.randint(0,len(colours))
+
+	for j in range(8):
+		newCols.append( (159, (int)(colours[(baseIndex+j*gap)%len(colours)]) ) )
+	palette.append(newCols)
+print 'Pallette Selection complete'
+
+inputs = (
+		("CRATER", "label"),
+		("Seed:", 0),
+		("Smooth:", 4),
+		("Cache:",True),
+		("abrightmoore@yahoo.com.au", "label"),
+		("http://brightmoore.net", "label"),
+)
+
+def perform(level,box, options):
+	''' Feedback to abrightmoore@yahoo.com.au '''
+	# Local variables
+	method = "Perform"
+	(method, (width, height, depth), (centreWidth, centreHeight, centreDepth)) = FuncStart(level,box,options,method) # Log start
+
+	levelCopy = level
+	boxCopy = box
+
+	if options["Cache:"] == True:
+		levelCopy = level.extractSchematic(box) # Working set
+		boxCopy = BoundingBox((0,0,0),(width,height,depth))
+	
+	crater(levelCopy,boxCopy,options)
+
+	if options["Cache:"] == True:
+		level.copyBlocksFrom(levelCopy, boxCopy, (box.minx, box.miny, box.minz ))
+	
+	level.markDirtyBox(box)
+	FuncEnd(level,box,options,method) # Log end	
+	
+def crater(level,box,options):
+	method = "Crater"
+	(method, (width, height, depth), (centreWidth, centreHeight, centreDepth)) = FuncStart(level,box,options,method) # Log start
+	cw = int(centreWidth)
+	cd = int(centreDepth)
+	ch = int(centreHeight)
+	
+	print "Seed:"
+	SEED = int(options["Seed:"])
+	if SEED == 0:
+		SEED = randint(0,99999999999)
+	print SEED
+	RAND = Random(SEED)
+
+	# A crater has a bump in the middle, a basin with layers of material, subsidence rings, and a ring with ejecta lines.
+	
+	# Step 1 is to vaporise the material in the selection box in a hemisphere down to the middle of the box
+	THRESHHOLD = 0.4
+	a = centreWidth
+	b = centreHeight
+	c = centreDepth
+	for (x,y,z) in box.positions:
+		dx = (centreWidth-x)/1
+		dy = (centreHeight-y) #(height-1-y)/1
+		dz = (centreDepth-z)/1
+		wterm = (dx**2)/(a**2)
+		hterm = (dy**2)/(b**2)
+		dterm = (dz**2)/(c**2)
+		value = float(wterm + hterm + dterm)
+		# rings of material
+		dh = (wterm + dterm)
+		if value <= THRESHHOLD: # or dy > 0:
+			setBlockForced(level, AIR, box.minx+centreWidth+dx,box.miny+centreHeight+dy,box.minz+centreDepth+dz)
+		if dy > centreHeight/5:
+			ddy = 0
+		else:
+			ddy = (centreHeight/5-dy)*0.03
+		if dy > 0 and dy < centreHeight/5 and value > THRESHHOLD and value < THRESHHOLD+0.2+ddy: #THRESHHOLD+centreHeight/5-dy/5*0.01:
+			drawLineForced(level,(1,0), (box.minx+centreWidth+dx,box.miny+centreHeight+dy,box.minz+centreDepth+dz),(box.minx+centreWidth+dx,box.miny,box.minz))
+			# setBlockToGroundForced(level, (1,0), box.minx+centreWidth+dx,box.miny+centreHeight+dy,box.minz+centreDepth+dz)
+#		elif value-1 < 0.5 and dy > 1:
+#			setBlockForced(level, (1,0), box.minx+centreWidth+dx,box.maxy-1-dy-dh,box.minz+centreDepth+dz)
+	# Store the seed on a sign
+	(x,y,z) = (cw, ch, cd)
+	setBlock(level, (1,0), box.minx+x,box.miny+y,box.minz+z)
+	createSign(level, box.minx+x, box.miny+y+1, box.minz+z, str(SEED))
+
+#	block = getBlock(level, box.minx+x,box.miny+y+1,box.minz+z+1) # cleanup
+#	if block == GLASS: # an error
+#		setBlockForced(level, AIR, box.minx+x,box.miny+y+1,box.minz+z+1)
+						
+	FuncEnd(level,box,options,method)
+
+def drawLinesSmooth(level,box,material,SMOOTHAMOUNT,P):
+	Q = []
+	for i in xrange(0,SMOOTHAMOUNT):
+		P = chaikinSmoothAlgorithm(P)
+	Q = drawLines(level,box,material,P)
+	return Q
+
+def drawLinesSmoothForced(level,box,material,SMOOTHAMOUNT,P):
+	Q = []
+	for i in xrange(0,SMOOTHAMOUNT):
+		P = chaikinSmoothAlgorithm(P)
+	Q = drawLinesForced(level,box,material,P)
+	return Q
+	
+def drawLines(level,box,material,P):
+	Q = []
+	count = 0
+	(x0,y0,z0) = (0,0,0)
+	for (x,y,z) in P:
+		if count > 0:
+			Q.append( drawLine(level,material,(box.minx+x0,box.miny+y0,box.minz+z0),(box.minx+x,box.miny+y,box.minz+z)) )
+		count = count+1
+		(x0,y0,z0) = (x,y,z)
+	return Q
+
+def drawLinesForced(level,box,material,P):
+	Q = []
+	count = 0
+	(x0,y0,z0) = (0,0,0)
+	for (x,y,z) in P:
+		if count > 0:
+			Q.append( drawLineForced(level,material,(box.minx+x0,box.miny+y0,box.minz+z0),(box.minx+x,box.miny+y,box.minz+z)) )
+		count = count+1
+		(x0,y0,z0) = (x,y,z)
+	return Q
+	
+####################################### LIBS
+	
+def FuncStart(level, box, options, method):
+	# abrightmoore -> shim to prepare a function.
+	print '%s: Started at %s' % (method, time.ctime())
+	(width, height, depth) = (box.maxx - box.minx, box.maxy - box.miny, box.maxz - box.minz)
+	centreWidth = math.floor(width / 2)
+	centreHeight = math.floor(height / 2)
+	centreDepth = math.floor(depth / 2)	
+	# other initialisation methods go here
+	return (method, (width, height, depth), (centreWidth, centreHeight, centreDepth))
+
+def FuncEnd(level, box, options, method):
+	print '%s: Ended at %s' % (method, time.ctime())
+	
+def getBoxSize(box):
+	return (box.maxx - box.minx, box.maxy - box.miny, box.maxz - box.minz)
+
+def getBlockFromOptions(options, block):
+	return (options[block].ID, options[block].blockData)
+	
+# Ye Olde GFX Libraries
+def createSign(level, x, y, z, text): #abrightmoore - convenience method. Due to Jigarbov - this is not a Sign.
+	ALIASKEY = "SHIP NUMBER"
+	COMMANDBLOCK = 137
+	CHUNKSIZE = 16
+	STANDING_SIGN = 63
+	
+	setBlock(level, (STANDING_SIGN,8), x, y, z)
+	setBlock(level, (1,0), x, y-1, z)
+	control = TAG_Compound()
+	control["id"] = TAG_String("Sign")
+	control["Text1"] = TAG_String(ALIASKEY)
+	control["Text2"] = TAG_String(text)
+	control["Text3"] = TAG_String("Generated by")
+	control["Text4"] = TAG_String("@abrightmoore")	
+	
+	control["x"] = TAG_Int(x)
+	control["y"] = TAG_Int(y)
+	control["z"] = TAG_Int(z)
+	chunka = level.getChunk((int)(x/CHUNKSIZE), (int)(z/CHUNKSIZE))
+	chunka.TileEntities.append(control)
+	chunka.dirty = True
+
+def chaikinSmoothAlgorithm(P): # http://www.idav.ucdavis.edu/education/CAGDNotes/Chaikins-Algorithm/Chaikins-Algorithm.html
+	F1 = 0.25
+	F2 = 0.75
+	Q = []
+	(x0,y0,z0) = (-1,-1,-1)
+	count = 0
+	for (x1,y1,z1) in P:
+		if count > 0: # We have a previous point
+			(dx,dy,dz) = (x1-x0,y1-y0,z1-z0)
+#			Q.append( (x0*F2+x1*F1,0,z0*F2+z1*F1) )
+#			Q.append( (x0*F1+x1*F2,0,z0*F1+z1*F2) )
+
+			Q.append( (x0*F2+x1*F1,y0*F2+y1*F1,z0*F2+z1*F1) )
+			Q.append( (x0*F1+x1*F2,y0*F1+y1*F2,z0*F1+z1*F2) )
+
+#			Q.append( (x0+dx*F1+*F2,y0*F1+y1*F2,z0*F1+z1*F2) )
+#			Q.append( (x0*F2+x1*F1,y0*F2+y1*F1,z0*F2+z1*F1) )
+		else:
+			count = count+1
+		(x0,y0,z0) = (x1,y1,z1)
+
+	return Q
+
+def drawTriangle(level, (p1x, p1y, p1z), (p2x, p2y, p2z), (p3x, p3y, p3z), materialEdge, materialFill):
+	if materialFill != (0,0):
+		# for each step along the 'base' draw a line from the apex
+		dx = p3x - p2x
+		dy = p3y - p2y
+		dz = p3z - p2z
+
+		distHoriz = dx*dx + dz*dz
+		distance = sqrt(dy*dy + distHoriz)
+		
+		phi = atan2(dy, sqrt(distHoriz))
+		theta = atan2(dz, dx)
+
+		iter = 0
+		while iter <= distance:
+			(px, py, pz) = ((int)(p2x+iter*cos(theta)*cos(phi)), (int)(p2y+iter*sin(phi)), (int)(p2z+iter*sin(theta)*cos(phi)))
+			
+			iter = iter+0.5 # slightly oversample because I lack faith.
+			drawLine(level, materialFill, (px, py, pz), (p1x, p1y, p1z) )
+	
+	
+	drawLine(level, materialEdge, (p1x, p1y, p1z), (p2x, p2y, p2z) )
+	drawLine(level, materialEdge, (p1x, p1y, p1z), (p3x, p3y, p3z) )
+	drawLine(level, materialEdge, (p2x, p2y, p2z), (p3x, p3y, p3z) )
+
+def drawTriangleEdge(level, box, options, (p1x, p1y, p1z), (p2x, p2y, p2z), (p3x, p3y, p3z), materialEdge):
+	drawLine(level, materialEdge, (p1x, p1y, p1z), (p2x, p2y, p2z) )
+	drawLine(level, materialEdge, (p1x, p1y, p1z), (p3x, p3y, p3z) )
+	drawLine(level, materialEdge, (p2x, p2y, p2z), (p3x, p3y, p3z) )
+
+def plotLine(level,material,p1,p2):
+
+	(x1,y1,z1) = p1
+	(x2,y2,z2) = p2
+	
+	dx = x2-x1
+	dy = y2-y1
+	dz = z2-z1
+	
+	steps = abs(dx)
+	if abs(dy) > abs(dx):
+		steps = abs(dy)
+	if abs(dz) > abs(dy):
+		steps = abs(dz)
+	if steps == 0:
+		setBlock(level,material,x1,y1,z1)
+	else:
+		ddx = float(dx)/float(steps)
+		ddy = float(dy)/float(steps)
+		ddz = float(dz)/float(steps)
+		pdx = float(0)
+		pdy = float(0)
+		pdz = float(0)
+		
+		for i in xrange(0,int(steps)):
+			setBlock(level,material,x1+pdx,y1+pdy,z1+pdz)
+			pdx = pdx + ddx
+			pdy = pdy + ddy
+			pdz = pdz + ddz
+	
+def drawLine(scratchpad, (blockID, blockData), (x,y,z), (x1,y1,z1) ):
+	return drawLineConstrained(scratchpad, (blockID, blockData), (x,y,z), (x1,y1,z1), 0 )
+
+def drawLineForced(scratchpad, (blockID, blockData), (x,y,z), (x1,y1,z1) ):
+	return drawLineConstrainedForced(scratchpad, (blockID, blockData), (x,y,z), (x1,y1,z1), 0 )
+	
+def drawLine1(scratchpad, (blockID, blockData), (x,y,z), (x1,y1,z1) ):
+	for px, py, pz in bresenham.bresenham((x,y,z),(x1,y1,z1)):
+		setBlock(scratchpad,(blockID, blockData),px,py,pz)
+	setBlock(scratchpad,(blockID, blockData),x1,y1,z1)
+
+def drawLineConstrained(scratchpad, (blockID, blockData), (x,y,z), (x1,y1,z1), maxLength ):
+	dx = x1 - x
+	dy = y1 - y
+	dz = z1 - z
+
+	distHoriz = dx*dx + dz*dz
+	distance = sqrt(dy*dy + distHoriz)
+	P = []
+	if distance < maxLength or maxLength < 1:
+		phi = atan2(dy, sqrt(distHoriz))
+		theta = atan2(dz, dx)
+
+		iter = 0
+		while iter <= distance:
+			(xd,yd,zd) = ((int)(x+iter*cos(theta)*cos(phi)), (int)(y+iter*sin(phi)), (int)(z+iter*sin(theta)*cos(phi)))
+			setBlock(scratchpad,(blockID,blockData),xd,yd,zd)
+			P.append((xd,yd,zd))
+			iter = iter+0.5 # slightly oversample because I lack faith.
+	return P # The set of all the points drawn
+
+def drawLineConstrainedForced(scratchpad, (blockID, blockData), (x,y,z), (x1,y1,z1), maxLength ):
+	dx = x1 - x
+	dy = y1 - y
+	dz = z1 - z
+
+	distHoriz = dx*dx + dz*dz
+	distance = sqrt(dy*dy + distHoriz)
+	P = []
+	if distance < maxLength or maxLength < 1:
+		phi = atan2(dy, sqrt(distHoriz))
+		theta = atan2(dz, dx)
+
+		iter = 0
+		while iter <= distance:
+			(xd,yd,zd) = ((int)(x+iter*cos(theta)*cos(phi)), (int)(y+iter*sin(phi)), (int)(z+iter*sin(theta)*cos(phi)))
+			setBlockForced(scratchpad,(blockID,blockData),xd,yd,zd)
+			P.append((xd,yd,zd))
+			iter = iter+0.5 # slightly oversample because I lack faith.
+	return P # The set of all the points drawn
+
+	
+def drawLineConstrainedRandom(scratchpad, (blockID, blockData), (x,y,z), (x1,y1,z1), frequency ):
+	dx = x1 - x
+	dy = y1 - y
+	dz = z1 - z
+
+	distHoriz = dx*dx + dz*dz
+	distance = sqrt(dy*dy + distHoriz)
+
+
+	phi = atan2(dy, sqrt(distHoriz))
+	theta = atan2(dz, dx)
+
+	iter = 0
+	while iter <= distance:
+		if randint(0,99) < frequency:
+			scratchpad.setBlockAt((int)(x+iter*cos(theta)*cos(phi)), (int)(y+iter*sin(phi)), (int)(z+iter*sin(theta)*cos(phi)), blockID)
+			scratchpad.setBlockDataAt((int)(x+iter*cos(theta)*cos(phi)), (int)(y+iter*sin(phi)), (int)(z+iter*sin(theta)*cos(phi)), blockData)
+		iter = iter+0.5 # slightly oversample because I lack faith.
+	
+def getBlock(level,x,y,z):
+	return (level.blockAt(int(x),int(y),int(z)), level.blockDataAt(int(x),int(y),int(z)))
+
+def setBlock(level, (block, data), x, y, z):
+	if getBlock(level, x,y,z) == AIR:
+		level.setBlockAt(int(x), int(y), int(z), block)
+		level.setBlockDataAt(int(x), int(y), int(z), data)
+
+def setBlockToGroundForced(level, (block, data), x, y, z):
+	dy = y
+	while dy >= 0:
+		block = getBlock(level, x, dy, z)
+		if block == 0:
+			setBlockForced(level, (block, data), x, dy, z)
+		else:
+			dy = -1 # end
+		dy = dy-1
+		
+def setBlockForced(level, (block, data), x, y, z):
+	level.setBlockAt(int(x), int(y), int(z), block)
+	level.setBlockDataAt(int(x), int(y), int(z), data)
